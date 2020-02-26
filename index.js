d9a4fd10172feec0e32923d133b12aa181172e5b
Dennis <ukonudennis63@gmail.com>
2020-02-26 07:21:13 +0100

    third

--- index.js
+++ index.js
@@ -1,35 +1,16 @@
 const express = require("express");
-const passport = require("passport");
-const GoogleStrategy = require("passport-google-oauth20").Strategy;
-const keys = require("./config/keys");
-
 const app = express();
 
-app.get("/", (req, res) => {
-  res.send("i love you");
-});
+// Routes
+// const authRoutes = require("./routes/authRoutes");
+// authRoutes(app);
+//OR
+require("./routes/authRoutes")(app);
+require("./services/passport");
 
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: keys.googleClientID,
-      clientSecrets: keys.googleClientSecrets,
-      callbackUrl: "/auth/google/callback"
-    },
-    accessToken => {
-      console.log(accessToken);
-    }
-  )
-);
-
-app.get(
-  "/auth/google",
-  passport.authenticate("google", {
-    scope: ["profile", "email"]
-  })
-);
-
-app.get("/auth/google/callback", passport.authenticate("google"));
+// app.get("/", (req, res) => {
+//   res.send("i love you");
+// });
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
